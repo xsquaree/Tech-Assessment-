09cf79abbe745890079b9d030ef078f295059bbd
xsquaree <35002684+xsquaree@users.noreply.github.com>
2023-09-10 16:43:54 +0800

    Update Q3.py
    Ratings Assumption

--- Q3.py
+++ Q3.py
@@ -5,17 +5,17 @@ import csv
 
 df = pd.read_csv('restaurants.csv')
 
-
+#Assum
 def map_text_rating(rating):
-    if 0 <= rating < 1:
+    if 1 <= rating < 3:
         return 'Poor'
-    elif 1 <= rating < 2:
+    elif 3 <= rating < 3.5:
         return 'Average'
-    elif 2 <= rating < 3:
+    elif 3.5 <= rating < 4:
         return 'Good'
-    elif 3 <= rating < 4:
+    elif 4 <= rating < 4.5:
         return 'Very Good'
-    elif 4 <= rating <= 5:
+    elif 4.5 <= rating <= 5:
         return 'Excellent'
     else:
         return 'No rating'
@@ -23,6 +23,12 @@ def map_text_rating(rating):
 df['Text Rating'] = df['User Aggregate Rating'].apply(map_text_rating)
 
 aggregates = df['Text Rating'].value_counts()
+print("Threshold for ratings:") 
+print("4-5: Excellent")
+print("3-4: Very Good")
+print("2-3: Good")
+print("1-2: Average")
+
 
 print("Aggregates for the ratings: ")
 print(aggregates)
